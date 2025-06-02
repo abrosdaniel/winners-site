fce9b3b40fc25320d7fccffa29b876cc050aaf68
abrosxd <154523512+abrosxd@users.noreply.github.com>
2025-06-02 23:12:30 +0200

    Fix 6.3 | 02/06/2025

--- src/app/news/[id]/page.tsx
+++ src/app/news/[id]/page.tsx
@@ -7,17 +7,10 @@ type Props = {
   params: { id: string };
 };
 
-type NewsArticleProps = {
-  id: string;
-  title: string;
-  article: string;
-  image: string;
-};
-
 export const generateMetadata = async ({
   params,
 }: Props): Promise<Metadata> => {
-  const { id } = params;
+  const { id } = await params;
   const [news] = await directus.request(
     readItems("news", {
       filter: { id: { _eq: id }, status: { _eq: "published" } },
@@ -84,33 +77,16 @@ export const generateMetadata = async ({
   };
 };
 
-async function fetchArticle(id: string): Promise<NewsArticleProps | null> {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+export default async function NewsArticlePage({ params }: Props) {
+  const { id } = await params;
+  const [news] = await directus.request(
+    readItems("news", {
+      filter: { id: { _eq: id }, status: { _eq: "published" } },
+      fields: ["*"],
+    })
+  );
 
-  try {
-    const res = await fetch(`${baseUrl}/api/news/${id}`);
-    if (!res.ok) {
-      console.error("Failed to fetch article:", res.status, res.statusText);
-      return null;
-    }
-    const json = await res.json();
-    return json;
-  } catch (error) {
-    console.error("Error fetching article:", error);
-    return null;
-  }
-}
-
-export default async function NewsArticlePage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const article = await fetchArticle(params.id);
-
-  const getImageUrl = (fileId: string) => `/api/img/${fileId}`;
-
-  if (!article) {
+  if (!news) {
     return <div>Статья не найдена</div>;
   }
 
@@ -118,23 +94,24 @@ export default async function NewsArticlePage({
     <div className="w-full box-border relative flex flex-col items-center max-w-5xl mx-auto zoomer">
       <img
         className="w-full aspect-video object-cover object-top h-48 lg:h-[450px]"
-        src={getImageUrl(article.image)}
+        src={`${directus.url}assets/${news.image}`}
+        alt={news.title}
       />
       <div className="flex flex-col lg:flex-row lg:gap-4">
         <div className="px-2 lg:px-0 lg:w-4/6 mt-5 lg:mt-7">
           <h1 className="font-inter font-bold text-2xl leading-7 text-[#171D3D] lg:text-4xl mb-6">
-            {article.title}
+            {news.title}
           </h1>
           <div
             className="article font-inter font-normal text-base leading-5 text-black lg:text-lg mb-16"
-            dangerouslySetInnerHTML={{ __html: article.article }}
+            dangerouslySetInnerHTML={{ __html: news.article }}
           />
         </div>
         <div className="bg-[#F5F5F5] px-2 py-5 lg:w-2/6 lg:px-7 lg:py-7 flex flex-col gap-5 lg:gap-0 relative">
           <h2 className="font-bold text-4xl text-[#171D3D] lg:text-4xl lg:mb-4">
             Другие новости
           </h2>
-          <RecomendateArticle currentArticleId={article.id} />
+          <RecomendateArticle currentArticleId={news.id} />
           <div className="absolute bg-[#F5F5F5] h-16 w-full -bottom-16 left-0"></div>
         </div>
       </div>
